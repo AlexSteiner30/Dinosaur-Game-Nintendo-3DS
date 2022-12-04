6066ab1d84c2a833852e10a14d513ef121c8bb31
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-04 17:08:04 +0100

    Game Finished in 3 hours while being in a car

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI scoreTxt;
+    [SerializeField] private TextMeshProUGUI highScoreTxt;
     [SerializeField] private GameObject gameOver;
 
     [SerializeField] private Transform spawner;
@@ -13,13 +16,29 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] public float speed = 5;
     [SerializeField] private float spawningSpeed = 5;
+    [SerializeField] private int score = 0;
     [SerializeField] private bool alive;
 
     private void Start()
     {
         alive = true;
 
+        int highScore = PlayerPrefs.GetInt("HighScore");
+
+        if (highScore <= 9)
+            highScoreTxt.text = "0000" + highScore.ToString();
+        else if (highScore >= 10 && highScore <= 99)
+            highScoreTxt.text = "000" + highScore.ToString();
+        else if (highScore >= 100 && highScore <= 999)
+            highScoreTxt.text = "00" + highScore.ToString();
+        else if (highScore >= 1000 && highScore <= 9999)
+            highScoreTxt.text = "0" + highScore.ToString();
+        else if (highScore >= 10000 && highScore <= 99999)
+            highScoreTxt.text = highScore.ToString();
+
+
         StartCoroutine(Spawn());
+        StartCoroutine(Score());
     }
 
     IEnumerator Spawn()
@@ -43,12 +62,61 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    IEnumerator Score()
+    {
+        while(alive)
+        {
+            yield return new WaitForSeconds(0.1f);
+            score++;
+
+            if(score <= 9)
+                scoreTxt.text = "0000" + score.ToString();
+            else if(score >= 10 && score <= 99)
+                scoreTxt.text = "000" + score.ToString();
+            else if (score >= 100 && score <= 999)
+                scoreTxt.text = "00" + score.ToString();
+            else if (score >= 1000 && score <= 9999)
+                scoreTxt.text = "0" + score.ToString();
+            else if (score >= 10000 && score <= 99999)
+                scoreTxt.text = score.ToString();
+        }
+    }
+
     public void GameOver()
     {
         alive = false;
         Time.timeScale = 0f;
 
         gameOver.SetActive(true);
+
+
+        try
+        {
+            int highScore = PlayerPrefs.GetInt("HighScore");
+
+            if(score > highScore)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+
+                highScore = PlayerPrefs.GetInt("HighScore");
+
+                if (highScore <= 9)
+                    highScoreTxt.text = "0000" + highScore.ToString();
+                else if (highScore >= 10 && highScore <= 99)
+                    highScoreTxt.text = "000" + highScore.ToString();
+                else if (highScore >= 100 && highScore <= 999)
+                    highScoreTxt.text = "00" + highScore.ToString();
+                else if (highScore >= 1000 && highScore <= 9999)
+                    highScoreTxt.text = "0" + highScore.ToString();
+                else if (highScore >= 10000 && highScore <= 99999)
+                    highScoreTxt.text = highScore.ToString();
+            }
+        }
+
+        catch
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
 
         print("Game Over!");
 
@@ -63,5 +131,7 @@ public class GameManager : MonoBehaviour
         }
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        Time.timeScale = 2f;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,22 +6,41 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpHeight;
 
-    private void FixedUpdate()
+    [SerializeField] private bool isGrounded;
+
+    private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            print("A");
+            float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * rb.gravityScale));
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+            StartCoroutine(PlayJump());
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        isGrounded = true;
+        GetComponent<AudioSource>().mute = true;
+
         if (collision.gameObject.tag == "Obstacle")
         {
             gameManager.GameOver();
         }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+
+    IEnumerator PlayJump()
+    {
+        GetComponent<AudioSource>().mute = false;
+        yield return new WaitForSecondsRealtime(0.5f);
+        GetComponent<AudioSource>().mute = true;
     }
 }
